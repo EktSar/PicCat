f6656a47bdc1d06fc89492670e8a1b72b58736d0
EktSar <sarycheva789@gmail.com>
2020-07-29 17:27:13 +0500

    Updates

---
+++ precache-manifest.4a5150d030b2265c34d1e02eeeed5e19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fff377188c1e11baeb62d1e76fea40",
+    "url": "/PicCat/index.html"
+  },
+  {
+    "revision": "8c6b61dda3e388ecaaad",
+    "url": "/PicCat/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f0e243d33a15b5fd82e0",
+    "url": "/PicCat/static/css/main.50d17a9e.chunk.css"
+  },
+  {
+    "revision": "8c6b61dda3e388ecaaad",
+    "url": "/PicCat/static/js/2.fb2bcb30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PicCat/static/js/2.fb2bcb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e243d33a15b5fd82e0",
+    "url": "/PicCat/static/js/main.6da16a8e.chunk.js"
+  },
+  {
+    "revision": "1ef5c4faccadd70fa6d0",
+    "url": "/PicCat/static/js/runtime-main.c7df141d.js"
+  },
+  {
+    "revision": "47ab8fcfdfdf093819bfc1186886479d",
+    "url": "/PicCat/static/media/logo.47ab8fcf.png"
+  }
+]);
